20529935261058700446a168533977fadafb88ca
Aleksandar Milanovic <milanovicaleX77@gmail.com>
2022-01-22 20:40:03 +0100

    comment

--- util/regex/reg.go
+++ util/regex/reg.go
@@ -4,7 +4,7 @@ import (
 	"regexp"
 	"log"
 )
-
+// This need thinking, how to handle response from container
 func RegReplace(s string) string{
 	reg, err := regexp.Compile("[^a-zA-Z0-9_ .,]+")
     if err != nil {
